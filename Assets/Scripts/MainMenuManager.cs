d018e3edb23cb612b011fd6aec4930021be343b6
LChapman8 <1034370@student.sae.edu.au>
2026-04-09 11:04:44 +1000

    Mainmenu Creation

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene"); 
+    }
+
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game"); 
+        Application.Quit();
+    }
+}
